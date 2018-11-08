59244755e6edbf5ede34d47145265d22ad7f914d
Diego Lorca Chacon <diego.lorca.chacon@gmail.com>
2018-11-08 01:57:59 -0300

    add biography to user and add yarn of jquery bootstrap, also, add parameters in controller and views of devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181107174900) do
+ActiveRecord::Schema.define(version: 20181107194944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20181107174900) do
     t.string "district"
     t.string "photo"
     t.boolean "admin"
+    t.text "biography"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
